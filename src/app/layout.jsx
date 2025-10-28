056eb435a1e7966d9d35fbdc6dd6a6c3ffa6b1e8
Gabriel Akpagli <bssidd241792@mug.edu.gh>
2025-10-28 18:33:00 +0000

    jxxh

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,8 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-  <meta name="theme-color" content="#000000" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />  <meta name="theme-color" content="#000000" />
   <meta name="apple-mobile-web-app-capable" content="yes" />
   <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
   <meta name="apple-mobile-web-app-title" content="RAID ARENA" />
