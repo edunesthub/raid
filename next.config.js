107271904314a9faa98d10274517047fe364e290
Gabriel Akpagli <bssidd241792@mug.edu.gh>
2025-12-22 12:55:44 +0000

    fdf

--- next.config.js
+++ next.config.js
@@ -2,40 +2,37 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", // disables PWA in dev
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // 👇 THIS IS THE MONEY LINE
+  webpack: (config) => config,
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "placehold.co",
-        port: "",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-        port: "",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/**", // ✅ allow all Cloudinary paths
+        pathname: "/**",
       },
     ],
     formats: ["image/webp", "image/avif"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    unoptimized: false,
-  },
-  eslint: {
-    ignoreDuringBuilds: false,
   },
 };
 
---
+++ src/app/deposit/page.jsx
@@ -0,0 +1,4 @@
+// app/deposit/page.js
+export default function DepositPage() {
+  return <div>Deposit coming soon</div>;
+}
